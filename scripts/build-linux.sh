faeee0265e2e0bfb834f6f3934dff9b6a865ea3b
osa911 <e.osadchuy@gmail.com>
2025-07-01 13:00:44 +0100

    Fix build script paths

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run pyinstaller.sh and create-dmg.sh with paths relative to the script
 echo "Building for Linux..."
-sh "/app/scripts/pyinstaller.sh" && sh "/app/scripts/create-tar.gz.sh";
+sh "$SCRIPT_DIR/pyinstaller.sh" && sh "$SCRIPT_DIR/create-tar.gz.sh";
